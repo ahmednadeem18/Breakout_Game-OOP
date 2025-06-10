665dea61a60c4d1f436be8e71f4f41734fd65d12
ahmednadeem18 <bscs24129@itu.edu.pk>
2025-06-11 04:11:01 +0500

    powerup

--- PowerUp.h
+++ PowerUp.h
@@ -1,5 +1,30 @@
 #pragma once
-class PowerUp
-{
+#include "Paddle.h"
+#include "MyVector.h"
+#include "Ball.h"
+
+class PowerUp {
+public:
+    virtual void activate(Paddle* paddle, MyVector<Ball>* balls) = 0;
+    virtual ~PowerUp() {}
+};
+
+class ExpandPaddlePowerUp : public PowerUp {
+public:
+    void activate(Paddle* paddle, MyVector<Ball>* balls) override {
+        paddle->expand();
+    }
+};
+
+class MultiBallPowerUp : public PowerUp {
+public:
+    void activate(Paddle* paddle, MyVector<Ball>* balls) override {
+        if (balls->size() > 0) {
+            Ball b = balls->at(0);
+            b.setVelocityX(-b.getVelocityX());
+            b.setVelocityY(-b.getVelocityY());
+            balls->push(b);
+        }
+    }
 };
 
