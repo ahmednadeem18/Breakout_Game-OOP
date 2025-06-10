a1bf9a350d9f7e09ae0a983f98a905fbcb5ae2d3
ahmednadeem18 <bscs24129@itu.edu.pk>
2025-06-11 04:32:59 +0500

    power

--- PowerBrick.h
+++ PowerBrick.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "Brick.h"
+
+class PowerBrick : public Brick {
+public:
+    PowerBrick(int x, int y, int w, int h)
+        : Brick(x, y, w, h, 1) {
+    }
+
+    bool isPowerBrick() const override {
+        return true;
+    }
+};
