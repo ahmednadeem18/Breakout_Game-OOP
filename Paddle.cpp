c42df443e29da92101b7296b1978b87e49ae0a03
ahmednadeem18 <bscs24129@itu.edu.pk>
2025-06-11 03:57:55 +0500

    paddle

--- Paddle.cpp
+++ Paddle.cpp
@@ -1,2 +1,34 @@
 #include "Paddle.h"
 #include "raylib.h"
+#include "Paddle.h"
+#include "PowerUp.h"
+
+Paddle::Paddle(int startX, int startY, int w, int h, int s)
+    : x(startX), y(startY), width(w), height(h), speed(s) {
+}
+
+void Paddle::moveLeft() {
+    x -= speed;
+    if (x < 0) x = 0;
+}
+
+void Paddle::moveRight(int screenWidth) {
+    x += speed;
+    if (x + width > screenWidth) x = screenWidth - width;
+}
+
+void Paddle::draw() {
+    DrawRectangle(x, y, width, height, BLUE);
+}
+
+Rectangle Paddle::getRect() const {
+    return { (float)x, (float)y, (float)width, (float)height };
+}
+
+void Paddle::expand() {
+    width += 40;
+}
+
+void Paddle::catchPowerUp(PowerUp* power) {
+
+}
--- Paddle.h
+++ Paddle.h
@@ -1,6 +1,23 @@
 #pragma once
 #include "raylib.h"
-class Paddle
-{
+
+class PowerUp;
+
+class Paddle {
+private:
+    int x;
+    int y;
+    int width;
+    int height;
+    int speed;
+public:
+    Paddle(int startX, int startY, int w, int h, int s);
+    void moveLeft();
+    void moveRight(int screenWidth);
+    void draw();
+    Rectangle getRect() const;
+    void expand();
+    void catchPowerUp(PowerUp* power);
 };
+
 
