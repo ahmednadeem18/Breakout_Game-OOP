ce123face12a7dc8117884797d53aa86830124c2
ahmednadeem18 <bscs24129@itu.edu.pk>
2025-06-11 04:34:28 +0500

    powerup integrated

--- PowerBrick.h
+++ PowerBrick.h
@@ -1,13 +1,20 @@
 #pragma once
 #include "Brick.h"
+#include "PowerUp.h"
 
 class PowerBrick : public Brick {
+private:
+    PowerUp* power;
 public:
-    PowerBrick(int x, int y, int w, int h)
-        : Brick(x, y, w, h, 1) {
+    PowerBrick(int x, int y, int w, int h, PowerUp* p)
+        : Brick(x, y, w, h, 1), power(p) {
     }
 
     bool isPowerBrick() const override {
         return true;
+    }
+
+    PowerUp* releasePowerUp() {
+        return power;
     }
 };
