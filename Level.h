035b7c7169cbaa35afc4a95fd4c5aa05fe549174
ahmednadeem18 <bscs24129@itu.edu.pk>
2025-06-11 18:08:37 +0500

    level.h updated

--- Level.h
+++ Level.h
@@ -1,5 +1,23 @@
 #pragma once
-class Level
-{
-};
+#include "MyVector.h"
+#include "Brick.h"
+#include "Ball.h"
+#include "Paddle.h"
 
+class Level {
+private:
+    MyVector<Brick*> bricks;
+    MyVector<Ball> balls;
+    Paddle* paddle;
+    int number;
+public:
+    Level();
+    ~Level();
+    void load(int levelNumber);
+    void update(int screenWidth, int screenHeight);
+    void draw() const;
+    bool isCleared() const;
+    MyVector<Ball>& getBalls();
+    MyVector<Brick*>& getBricks();
+    Paddle* getPaddle();
+};
