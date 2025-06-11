99c9e1a1d31b7adeeccd2f2703d54575e3334816
ahmednadeem18 <bscs24129@itu.edu.pk>
2025-06-11 18:35:16 +0500

    Game.h updated

--- Game.h
+++ Game.h
@@ -1,5 +1,21 @@
 #pragma once
-class Game
-{
-};
+#include "MyVector.h"
+#include "Level.h"
+#include "MyStr.h"
+#include "Logger.h"
 
+class Game {
+private:
+    MyVector<Level> levels;
+    int score;
+    int currentLevel;
+    Logger& logger;
+public:
+    Game();
+    void start(int screenWidth, int screenHeight);
+    void handleInput();
+    void update(int screenWidth, int screenHeight);
+    void draw() const;
+    void loadGame(const MyStr& filename);
+    void saveGame(const MyStr& filename);
+};
