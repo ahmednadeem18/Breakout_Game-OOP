ede435b3657d72b4b247e2fdaffd0070b053a22f
ahmednadeem18 <bscs24129@itu.edu.pk>
2025-06-14 03:25:40 +0500

    Removed these and fix them in brick bcz so much Files

--- HardBrick.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Brick.h"
-
-class HardBrick : public Brick {
-public:
-    HardBrick(int x, int y, int w, int h)
-        : Brick(x, y, w, h, 2) {
-    }
-};
--- NormalBrick.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Brick.h"
-
-class NormalBrick : public Brick {
-public:
-    NormalBrick(int x, int y, int w, int h)
-        : Brick(x, y, w, h, 1) {
-    }
-};
