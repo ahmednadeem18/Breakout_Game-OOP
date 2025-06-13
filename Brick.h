223b538582a657f79ccdd82d550e30e8e2897512
ahmednadeem18 <bscs24129@itu.edu.pk>
2025-06-14 03:27:53 +0500

    Brick.h impleted with its Types

--- Brick.h
+++ Brick.h
@@ -1,24 +1,57 @@
-#pragma once
+#ifndef BRICK_H
+#define BRICK_H
+
+#include "MyStr.h"
 #include <raylib.h>
 
 class Brick {
 protected:
-    int x;
-    int y;
-    int width;
-    int height;
-    int health;
-public:
-    Brick(int x, int y, int w, int h, int hp);
-    virtual void hit();
-    virtual bool isDestroyed() const;
-    virtual void draw() const;
-    virtual bool isPowerBrick() const;
-    Rectangle getRect() const;
-    virtual ~Brick() {}
-    int getX() const;
-    int getY() const;
-    int getWidth() const;
-    int getHeight() const;
+    Rectangle rectangle;
+    bool isDestroyed;
 
+public:
+    Brick(float x, float y, float width = 60, float height = 20);
+    virtual ~Brick() {}
+    virtual void hit() = 0;
+    virtual void draw() = 0;
+    virtual bool getIsDestroyed() const;
+    virtual MyStr getType() const = 0;
+    Rectangle getRectangle() const;
 };
+
+class NormalBrick : public Brick {
+public:
+    NormalBrick(float x, float y);
+    void hit() override;
+    void draw() override;
+    MyStr getType() const override;
+};
+
+class HardBrick : public Brick {
+    int hp;
+public:
+    HardBrick(float x, float y);
+    void hit() override;
+    void draw() override;
+    MyStr getType() const override;
+};
+
+class UnbreakableBrick : public Brick {
+public:
+    UnbreakableBrick(float x, float y);
+    void hit() override;
+    void draw() override;
+    MyStr getType() const override;
+};
+
+class PowerBrick : public Brick {
+    bool powerReleased;
+public:
+    PowerBrick(float x, float y);
+    void hit() override;
+    void draw() override;
+    MyStr getType() const override;
+    bool isPowerReleased() const;
+};
+
+#endif
