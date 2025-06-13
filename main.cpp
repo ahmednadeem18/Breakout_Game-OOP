1833b65cbe403c76c2678aea8221c466d351b84f
ahmednadeem18 <bscs24129@itu.edu.pk>
2025-06-14 03:32:16 +0500

    simple main calling Game functions update.

--- main.cpp
+++ main.cpp
@@ -1,65 +1,19 @@
-#include "raylib.h"
-#include "Level.h"
-#include "Logger.h"
-#include "SaveLoadManager.h"
-#include "MyStr.h"
-#include "MyVector.h"
-
-Level currentLevel;
-int currentLevelIndex = 0;
-Logger& logger = Logger::getInstance();
-
-void handleInput() {
-    Paddle* paddle = currentLevel.getPaddle();
-    if (IsKeyDown(KEY_LEFT)) paddle->moveLeft();
-    if (IsKeyDown(KEY_RIGHT)) paddle->moveRight(GetScreenWidth());
-    if (IsKeyPressed(KEY_S)) SaveLoadManager::saveLevel(currentLevel, "save.bin");
-    if (IsKeyPressed(KEY_L)) SaveLoadManager::loadLevel(currentLevel, "save.bin");
-}
-
-void update(int screenWidth, int screenHeight) {
-    currentLevel.update(screenWidth, screenHeight);
-
-    if (currentLevel.getBalls().size() == 0) {
-        BeginDrawing();
-        ClearBackground(BLACK);
-        DrawText("GAME OVER", 300, 250, 40, RED);
-        EndDrawing();
-        double start = GetTime();
-        while (GetTime() - start < 2.0) {}
-        CloseWindow();
-    }
-
-    if (currentLevel.isCleared()) {
-        currentLevelIndex++;
-        Level next;
-        next.load(currentLevelIndex);
-        currentLevel = next;
-    }
-}
-
-void draw() {
-    currentLevel.draw();
-}
+#include "Game.h"
 
 int main() {
-    InitWindow(800, 600, "BreakOut");
-    SetTargetFPS(60);
-    logger.initialize("log.txt");
+    const int screenWidth = 800;
+    const int screenHeight = 600;
 
-    currentLevel.load(0);
+    InitWindow(screenWidth, screenHeight, "Brick Breaker");
+    SetTargetFPS(60);
+
+    Game game;
 
     while (!WindowShouldClose()) {
-        handleInput();
-        update(800, 600);
-
-        BeginDrawing();
-        ClearBackground(BLACK);
-        draw();
-        EndDrawing();
+        game.update();
+        game.draw();
     }
 
-    logger.cleanup();
     CloseWindow();
     return 0;
 }
