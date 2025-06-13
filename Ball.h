30475f34b4680a7560d9f820e67eb74c5da9f0b1
ahmednadeem18 <bscs24129@itu.edu.pk>
2025-06-14 03:24:09 +0500

    ball.h updated using position 2d

--- Ball.h
+++ Ball.h
@@ -1,27 +1,25 @@
-#pragma once
+#ifndef BALL_H
+#define BALL_H
+
+#include "Position2D.h"
 #include <raylib.h>
-#include "Brick.h"
-#include "Paddle.h"
 
 class Ball {
 private:
-    float x;
-    float y;
-    float velocityX;
-    float velocityY;
+    Position2D position;
+    Position2D velocity;
     float radius;
+
 public:
     Ball();
-    Ball(float startX, float startY, float speedX, float speedY, float r);
-    void update();
-    void draw() const;
-    bool checkBrickCollision(Brick* brick);
-    bool checkWallCollision(int screenWidth, int screenHeight);
-    bool checkPaddleCollision(Paddle* paddle);
-    float getX() const;
-    float getY() const;
-    float getVelocityX() const;
-    float getVelocityY() const;
-    void setVelocityX(float vx);
-    void setVelocityY(float vy);
+    void reset();
+    void update( );
+    void draw();
+    bool checkCollision(Rectangle rect);
+    bool isBelowScreen();
+    Position2D& getPosition();
+    Position2D& getVelocity();
+    void setVelocity(Position2D newVel);
 };
+
+#endif
